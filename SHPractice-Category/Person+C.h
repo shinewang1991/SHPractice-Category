50c531a67fb1794e95608df2759c28b12f6e574a
wangxiang <wangxiang@yixia.com>
2018-12-15 21:42:44 +0800

    add category Person+C

---
+++ SHPractice-Category/Person+C.h
@@ -0,0 +1,13 @@
+//
+//  Person+C.h
+//  SHPractice-Category
+//
+//  Created by Shine on 2018/12/15.
+//  Copyright © 2018年 shine. All rights reserved.
+//
+
+#import "Person.h"
+
+@interface Person (C)
++ (void)load;
+@end
---
+++ SHPractice-Category/Person+C.m
@@ -0,0 +1,17 @@
+//
+//  Person+C.m
+//  SHPractice-Category
+//
+//  Created by Shine on 2018/12/15.
+//  Copyright © 2018年 shine. All rights reserved.
+//
+
+#import "Person+C.h"
+
+@implementation Person (C)
+
+
++ (void)load{
+    NSLog(@"PersonC load");
+}
+@end
